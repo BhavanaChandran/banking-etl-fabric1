1bd6e508f264988fa3c4d107c4265eb88fba62aa
Bhavana Chandran <bhavanachandran6@gmail.com>
2026-07-13 13:57:16 +0530

    Initial folder structure and data load

---
+++ notebooks/load_data.py
@@ -0,0 +1,4 @@
+import pandas as pd
+df = pd.read_csv('C:/Users/bhavana.chandran/OneDrive - WAY POINT Systems fzco/DATAENGINEER/Projects/banking-etl-fabric1/data/PS_20174392719_1491204439457_log.csv')
+print(df.shape)
+print(df.dtypes)
